78e46b0ee3c836ec52715c208775695293562f69
Ricardo Arguello <ricardo.arguello@soportelibre.com>
2017-07-28 16:06:27 -0500

    Se mejora la validacion de PDF.

--- src/main/java/ec/gob/firmadigital/servicio/pdf/ServicioValidacionPdf.java
+++ src/main/java/ec/gob/firmadigital/servicio/pdf/ServicioValidacionPdf.java
@@ -46,13 +46,12 @@ import ec.gob.firmadigital.servicio.util.Base64InvalidoException;
 import ec.gob.firmadigital.servicio.util.Base64Util;
 import io.rubrica.certificate.ec.bce.CertificadoBancoCentral;
 import io.rubrica.certificate.ec.bce.CertificadoBancoCentralFactory;
-import io.rubrica.certificate.ec.securitydata.CertificadoSecurityData;
-import io.rubrica.certificate.ec.securitydata.CertificadoSecurityDataFactory;
 import io.rubrica.ocsp.OcspValidationException;
-import io.rubrica.sign.Falla;
-import io.rubrica.sign.Verificacion;
-import io.rubrica.sign.pdf.Firma;
-import io.rubrica.sign.pdf.VerificadorFirmaPdf;
+import io.rubrica.sign.InvalidFormatException;
+import io.rubrica.sign.SignInfo;
+import io.rubrica.sign.Signer;
+import io.rubrica.sign.pdf.PDFSigner;
+import io.rubrica.util.Utils;
 
 /**
  * Servicio de verificacion de archivos PDF.
@@ -68,37 +67,26 @@ public class ServicioValidacionPdf {
 
     private static final Logger logger = Logger.getLogger(ServicioValidacionPdf.class.getName());
 
-    public String getNombre(byte[] pdf) throws SignatureException, OcspValidationException, KeyStoreException {
-        try {
-            VerificadorFirmaPdf verificador = new VerificadorFirmaPdf(pdf);
-            Verificacion verificacion = verificador.verificar();
-            List<Firma> firmas = verificacion.getFirmas();
+    public String getNombre(byte[] pdf) throws IOException, InvalidFormatException, OcspValidationException {
+        Signer signer = new PDFSigner();
+        List<SignInfo> singInfos = signer.getSigners(pdf);
 
-            if (!firmas.isEmpty()) {
-                Firma firma = firmas.get(0);
-                X509Certificate certificado = firma.getCertificadoFirmante();
+        if (!singInfos.isEmpty()) {
+            SignInfo firma = singInfos.get(0);
+            X509Certificate certificado = firma.getCerts()[0];
 
-                logger.info("Verificando CRL local del certificado");
-                boolean revocado = servicioCrl.isRevocado(certificado.getSerialNumber());
-                logger.info("revocado=" + revocado);
+            logger.info("Verificando CRL local del certificado");
+            boolean revocado = servicioCrl.isRevocado(certificado.getSerialNumber());
+            logger.info("revocado=" + revocado);
 
-                if (revocado) {
-                    // FIXME
-                    throw new OcspValidationException();
-                }
-
-                if (CertificadoBancoCentralFactory.esCertificadoDelBancoCentral(certificado)) {
-                    CertificadoBancoCentral bce = CertificadoBancoCentralFactory.construir(certificado);
-                    return bce.getNombres() + " " + bce.getPrimerApellido() + " " + bce.getSegundoApellido();
-                } else if (CertificadoSecurityDataFactory.esCertificadoDeSecurityData(certificado)) {
-                    CertificadoSecurityData sd = CertificadoSecurityDataFactory.construir(certificado);
-                    return sd.getNombres() + " " + sd.getPrimerApellido() + " " + sd.getSegundoApellido();
-                }
+            if (revocado) {
+                // FIXME
+                throw new OcspValidationException();
             }
 
+            return Utils.getCN(certificado);
+        } else {
             return "Unknown";
-        } catch (IOException e) {
-            throw new RuntimeException("Error al verificar", e);
         }
     }
 
@@ -116,54 +104,48 @@ public class ServicioValidacionPdf {
             return Response.status(Status.BAD_REQUEST).entity("Error al decodificar Base64").build();
         }
 
-        Verificacion verificacion;
+        Signer signer = new PDFSigner();
+        List<SignInfo> firmas;
 
         try {
-            VerificadorFirmaPdf verificador = new VerificadorFirmaPdf(pdf);
-            verificacion = verificador.verificar();
-        } catch (IOException e) {
+            firmas = signer.getSigners(pdf);
+        } catch (InvalidFormatException | IOException e) {
             return Response.status(Status.BAD_REQUEST).entity("Error al verificar PDF: \"" + e.getMessage() + "\"")
                     .build();
         }
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-
         // Para construir un array de firmantes
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
-        List<Firma> firmas = verificacion.getFirmas();
-
-        for (Firma firma : firmas) {
-            X509Certificate certificado = firma.getCertificadoFirmante();
-            Falla falla = firma.getFalla();
+        for (SignInfo firma : firmas) {
+            JsonObjectBuilder builder = Json.createObjectBuilder();
+            X509Certificate certificado = firma.getCerts()[0];
 
             if (CertificadoBancoCentralFactory.esCertificadoDelBancoCentral(certificado)) {
                 CertificadoBancoCentral bce = CertificadoBancoCentralFactory.construir(certificado);
-
-                if (falla == null) {
-                    JsonObjectBuilder builder = Json.createObjectBuilder();
-                    builder.add("fecha", sdf.format(firma.getFechaFirma().getTime()));
-                    builder.add("cedula", bce.getCedulaPasaporte());
-                    builder.add("nombre",
-                            bce.getNombres() + " " + bce.getPrimerApellido() + " " + bce.getSegundoApellido());
-                    builder.add("cargo", bce.getCargo());
-                    builder.add("institucion", bce.getInstitucion());
-                    arrayBuilder.add(builder);
-                } else {
-                    JsonObjectBuilder builder = Json.createObjectBuilder();
-                    builder.add("fecha", sdf.format(firma.getFechaFirma().getTime()));
-                    builder.add("cedula", bce.getCedulaPasaporte());
-                    builder.add("nombre",
-                            bce.getNombres() + " " + bce.getPrimerApellido() + " " + bce.getSegundoApellido());
-                    builder.add("cargo", bce.getCargo());
-                    builder.add("institucion", bce.getInstitucion());
-
-                    String mensaje = falla.getMensaje();
-                    builder.add("mensaje", mensaje);
-
-                    arrayBuilder.add(builder);
-                }
+                builder.add("fecha", sdf.format(firma.getSigningTime()));
+                builder.add("cedula", bce.getCedulaPasaporte());
+                builder.add("nombre",
+                        bce.getNombres() + " " + bce.getPrimerApellido() + " " + bce.getSegundoApellido());
+                builder.add("cargo", bce.getCargo());
+                builder.add("institucion", bce.getInstitucion());
+                arrayBuilder.add(builder);
             }
+
+            // TODO: Implementar
+
+            /*
+             * if (CertificadoSecurityDataFactory.esCertificadoDeSecurityData(
+             * certificado)) { CertificadoSecurityData sd =
+             * CertificadoSecurityDataFactory.construir(certificado);
+             * builder.add("fecha", sdf.format(firma.getSigningTime()));
+             * builder.add("cedula", sd.getCedulaPasaporte());
+             * builder.add("nombre", sd.getNombres() + " " +
+             * sd.getPrimerApellido() + " " + sd.getSegundoApellido());
+             * builder.add("cargo", sd.getCargo()); builder.add("institucion",
+             * sd.getInstitucion()); arrayBuilder.add(builder); }
+             */
         }
 
         // Construir JSON
