5b2fbce7704f51fbfc2aaa17d3bcde7c023eeffd
Ricardo Arguello <ricardo.arguello@soportelibre.com>
2017-09-14 15:09:09 -0500

    Se agrega un servicio de verificación de revocados contra la base de datos local

---
+++ src/main/java/ec/gob/firmadigital/servicio/rest/ServicioCertificadoRest.java
@@ -0,0 +1,51 @@
+/*
+ * Firma Digital: Servicio
+ * Copyright 2017 Secretaría Nacional de la Administración Pública
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ec.gob.firmadigital.servicio.rest;
+
+import java.math.BigInteger;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import ec.gob.firmadigital.servicio.crl.ServicioCrl;
+
+/**
+ * Este servicio permite verificar si un certificado está revocado.
+ * 
+ * @author Ricardo Arguello <ricardo.arguello@soportelibre.com>
+ */
+@Stateless
+@Path("/certificado")
+public class ServicioCertificadoRest {
+
+    @EJB
+    private ServicioCrl servicioCrl;
+
+    @GET
+    @Path("/revocado/{serial}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String validarCertificado(@PathParam("serial") Integer serial) {
+        return Boolean.valueOf(servicioCrl.isRevocado(BigInteger.valueOf(serial))).toString();
+    }
+}
